478ef6c3c5a16dc6bf67ca96c0c274f28519202e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-27 22:32:16 +0100

    Add file

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/Tests/Core.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoNetworking");
+
+    return theTestHarness.run();
+}
