62ed0f3dd3892eb3f6bb11f3665d3b3cff3b3a49
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-30 22:48:00 +0100

    Add files

---
+++ include/Ishiko/Networking/IPv6Address.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_NETWORKING_IPV6ADDRESS_H_
+#define _ISHIKO_NETWORKING_IPV6ADDRESS_H_
+
+#endif
