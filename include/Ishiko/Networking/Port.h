91692c9e64ef85452bee2ef036f137a37f08f6af
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-31 00:06:04 +0100

    Add files

---
+++ include/Ishiko/Networking/Port.h
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_NETWORKING_PORT_H_
+#define _ISHIKO_NETWORKING_PORT_H_
+
+namespace Ishiko
+{
+namespace Networking
+{
+
+class Port
+{
+};
+
+}
+}
+
+#endif
