056c9e159ac9171e907f23691d0883a73e54995c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-10 10:29:35 +0000

    Implement and test Linux server socket

--- src/linux/TCPClientSocket.cpp
+++ src/linux/TCPClientSocket.cpp
@@ -5,6 +5,10 @@
 */
 
 #include "linux/TCPClientSocket.hpp"
+#include "ErrorCategory.hpp"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 namespace Ishiko
 {
@@ -13,16 +17,42 @@ namespace Networking
 
 TCPClientSocket::TCPClientSocket(Error& error)
 {
-    // TODO
+    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (m_socket == -1)
+    {
+        // TODO: more detailed error
+        Fail(error, ErrorCategory::Value::generic, "", __FILE__, __LINE__);
+        return;
+    }
+}
+
+TCPClientSocket::TCPClientSocket(int socket)
+    : m_socket(socket)
+{
 }
 
 TCPClientSocket::~TCPClientSocket()
 {
+    if (m_socket != -1)
+    {
+        close(m_socket);
+    }
 }
 
 void TCPClientSocket::connect(IPv4Address address, Port port, Error& error)
 {
-    // TODO
+    sockaddr_in linuxAddress;
+    linuxAddress.sin_family = AF_INET;
+    linuxAddress.sin_port = htons(port.number());
+    linuxAddress.sin_addr.s_addr = htonl(address.value());
+
+    int err = ::connect(m_socket, reinterpret_cast<sockaddr*>(&linuxAddress), sizeof(linuxAddress));
+    if (m_socket == -1)
+    {
+        // TODO: more detailed error
+        Fail(error, ErrorCategory::Value::generic, "", __FILE__, __LINE__);
+        return;
+    }
 }
 
 }
