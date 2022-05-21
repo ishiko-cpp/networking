1edc0825ab9b27278f17e0428d840248816abf74
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-21 14:20:55 +0100

    Refactoring

--- include/Ishiko/Networking/TLSClientSocket.hpp
+++ include/Ishiko/Networking/TLSClientSocket.hpp
@@ -10,6 +10,9 @@
 #include "IPv4Address.hpp"
 #include "Port.hpp"
 #include "TCPClientSocket.hpp"
+#include <botan/auto_rng.h>
+#include <botan/tls_callbacks.h>
+#include <botan/tls_session_manager.h>
 #include <Ishiko/Errors.hpp>
 
 namespace Ishiko
@@ -22,8 +25,34 @@ public:
 
     void connect(IPv4Address address, Port port, Error& error) noexcept;
 
+    // TODO: should length be size_t, same for return value
+    int read(char* buffer, int length, Error& error);
+    // TODO: always blocking at the moment, if this is non-blocking then would need to return the actual number of
+    // bytes sent
+    // TODO: should length be size_t
+    void write(const char* buffer, int length, Error& error);
+
 private:
+    class BotanTLSCallbacks : public Botan::TLS::Callbacks
+    {
+    public:
+        BotanTLSCallbacks(TCPClientSocket& socket);
+
+        void tls_emit_data(const uint8_t data[], size_t size) override;
+        void tls_record_received(uint64_t seq_no, const uint8_t data[], size_t size) override;
+        void tls_alert(Botan::TLS::Alert alert) override;
+        bool tls_session_established(const Botan::TLS::Session& session) override;
+
+    private:
+        TCPClientSocket& m_socket;
+    };
+
     TCPClientSocket m_socket;
+    BotanTLSCallbacks m_botanTLSCallbacks;
+    // TODO: what is the autoseed RNG, is it safe enough
+    Botan::AutoSeeded_RNG m_rng;
+    // TODO: do I need this although I said the session is never cached above?
+    Botan::TLS::Session_Manager_In_Memory m_sessionManager;
 };
 
 }
--- src/TLSClientSocket.cpp
+++ src/TLSClientSocket.cpp
@@ -5,53 +5,14 @@
 */
 
 #include "TLSClientSocket.hpp"
-
-#include <botan/auto_rng.h>
 #include <botan/certstor_system.h>
-#include <botan/tls_callbacks.h>
 #include <botan/tls_client.h>
 #include <botan/tls_policy.h>
-#include <botan/tls_session_manager.h>
 
 using namespace Ishiko;
 
 namespace
 {
-
-class BotanTLSCallbacks : public Botan::TLS::Callbacks
-{
-public:
-    BotanTLSCallbacks(TCPClientSocket& socket)
-        : m_socket(socket)
-    {
-    }
-
-    void tls_emit_data(const uint8_t data[], size_t size) override
-    {
-        // TODO: how do I handle errors
-        Error error;
-        m_socket.write((const char*)data, size, error);
-    }
-
-    void tls_record_received(uint64_t seq_no, const uint8_t data[], size_t size) override
-    {
-        // TODO: how do I pass this back synchronously to the application
-    }
-
-    void tls_alert(Botan::TLS::Alert alert) override
-    {
-        // TODO: how do I handle all possible alerts. One of those is in fact "close"
-    }
-
-    bool tls_session_established(const Botan::TLS::Session& session) override
-    {
-        // Do not cache the session
-        // TODO: do I need to support this?
-        return false;
-    }
-
-    TCPClientSocket& m_socket;
-};
 
 // TODO: I need to put this somewhere else and make user configurable
 class BotanCredentialsManager : public Botan::Credentials_Manager
@@ -90,8 +51,37 @@ private:
 
 }
 
+TLSClientSocket::BotanTLSCallbacks::BotanTLSCallbacks(TCPClientSocket& socket)
+    : m_socket(socket)
+{
+}
+
+void TLSClientSocket::BotanTLSCallbacks::tls_emit_data(const uint8_t data[], size_t size)
+{
+    // TODO: how do I handle errors
+    Error error;
+    m_socket.write((const char*)data, size, error);
+}
+
+void TLSClientSocket::BotanTLSCallbacks::tls_record_received(uint64_t seq_no, const uint8_t data[], size_t size)
+{
+    // TODO: how do I pass this back synchronously to the application
+}
+
+void TLSClientSocket::BotanTLSCallbacks::tls_alert(Botan::TLS::Alert alert)
+{
+    // TODO: how do I handle all possible alerts. One of those is in fact "close"
+}
+
+bool TLSClientSocket::BotanTLSCallbacks::tls_session_established(const Botan::TLS::Session& session)
+{
+    // Do not cache the session
+    // TODO: do I need to support this?
+    return false;
+}
+
 TLSClientSocket::TLSClientSocket(Error& error) noexcept
-    : m_socket(error)
+    : m_socket(error), m_botanTLSCallbacks(m_socket), m_sessionManager(m_rng)
 {
 }
 
@@ -99,13 +89,7 @@ void TLSClientSocket::connect(IPv4Address address, Port port, Error& error) noex
 {
     m_socket.connect(address, port, error);
 
-    BotanTLSCallbacks callbacks(m_socket);
-
-    // TODO: what is the autoseed RNG, is it safe enough
-    Botan::AutoSeeded_RNG rng;
-
-    // TODO: do I need this although I said the session is never cached above?
-    Botan::TLS::Session_Manager_In_Memory sessionManager(rng);
+    
     
     BotanCredentialsManager credentials;
     
@@ -114,7 +98,7 @@ void TLSClientSocket::connect(IPv4Address address, Port port, Error& error) noex
     // TODO: replace "needfulsoftware.com" with correct domain but how?
     // TODO: I want TLS v.1.3 but only available in Botan 3.0.0. which is not released yet
     // TODO: what of TLS 1.2 is not supported, can I safely downgrade?
-    Botan::TLS::Client tlsClient(callbacks, sessionManager, credentials, policy, rng,
+    Botan::TLS::Client tlsClient(m_botanTLSCallbacks, m_sessionManager, credentials, policy, m_rng,
         Botan::TLS::Server_Information("needfulsoftware.com", port.number()),
         Botan::TLS::Protocol_Version::TLS_V12);
     // When the Botan TLS client is active it means the handshake has finished and we can start sending data so we
@@ -126,6 +110,23 @@ void TLSClientSocket::connect(IPv4Address address, Port port, Error& error) noex
         // TODO: handle error
         int n = m_socket.read(buffer, sizeof(buffer), error);
 
-        tlsClient.received_data((const uint8_t*)buffer, n);
+        // TODO: is there not an issue that I may read data from the server past the handshake. So application data
+        // might be called. This would only happen if the server pushes data. So I need to buffer everything received
+        // by tls_record_received
+
+        // TODO: this returns the amount of data needed to complete the next record. Do I need to do something with
+        // this.
+        size_t needed = tlsClient.received_data((const uint8_t*)buffer, n);
     }
+}
+
+int TLSClientSocket::read(char* buffer, int length, Error& error)
+{
+    // TODO: how much TLS encoded data do I need to read to get the amount requested by the client?
+    // I'll never know I think. So I can only read into an internal buffer and return data from that.
+    return 0;
+}
+
+void TLSClientSocket::write(const char* buffer, int length, Error& error)
+{
 }
