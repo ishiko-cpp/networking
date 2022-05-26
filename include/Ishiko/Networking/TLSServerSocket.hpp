e8d65513d9c4ef22c05ac825c9fce5a2e938d9df
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-26 17:35:00 +0100

    Add files

---
+++ include/Ishiko/Networking/TLSServerSocket.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP_
+#define _ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP_
+
+#endif
