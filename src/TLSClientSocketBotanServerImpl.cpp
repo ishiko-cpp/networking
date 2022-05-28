b3519848fd64db9f833d95ae49efe60d9778b658
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-28 12:32:25 +0100

    Add files

---
+++ src/TLSClientSocketBotanServerImpl.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
+*/
+
+#include "TLSClientSocketBotanServerImpl.hpp"
