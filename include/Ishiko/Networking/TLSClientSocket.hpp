40ae2d518dc187f141738b1d88b2a4cf0ceb9ed2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-28 12:17:58 +0100

    Refactoring

--- include/Ishiko/Networking/TLSClientSocket.hpp
+++ include/Ishiko/Networking/TLSClientSocket.hpp
@@ -9,14 +9,6 @@
 
 #include "IPv4Address.hpp"
 #include "Port.hpp"
-#include "TCPClientSocket.hpp"
-#undef min  // TODO: track down where I include <windows.h> and sort this out
-#include <botan/auto_rng.h>
-#include <botan/certstor_system.h>
-#include <botan/tls_callbacks.h>
-#include <botan/tls_client.h>
-#include <botan/tls_policy.h>
-#include <botan/tls_session_manager.h>
 #include <Ishiko/Errors.hpp>
 #include <string>
 
@@ -27,6 +19,7 @@ class TLSClientSocket
 {
 public:
     TLSClientSocket(Error& error) noexcept;
+    ~TLSClientSocket();
 
     // TODO: hostname is for SNI. Should I provide an overload for when SNI is not needed? The caller can just leave it
     // empty though
@@ -40,53 +33,17 @@ public:
     void write(const char* buffer, int length, Error& error);
 
 private:
-    class BotanTLSCallbacks : public Botan::TLS::Callbacks
+    friend class TLSClientSocketBotanClientImpl;
+
+    class Impl
     {
     public:
-        BotanTLSCallbacks(TCPClientSocket& socket, std::string& buffer);
-
-        void tls_emit_data(const uint8_t data[], size_t size) override;
-        void tls_record_received(uint64_t seq_no, const uint8_t data[], size_t size) override;
-        void tls_alert(Botan::TLS::Alert alert) override;
-        bool tls_session_established(const Botan::TLS::Session& session) override;
-        void tls_verify_cert_chain(const std::vector<Botan::X509_Certificate>& cert_chain,
-            const std::vector<std::shared_ptr<const Botan::OCSP::Response>>& ocsp,
-            const std::vector<Botan::Certificate_Store*>& trusted_roots, Botan::Usage_Type usage,
-            const std::string& hostname, const Botan::TLS::Policy& policy) override;
-
-    private:
-        TCPClientSocket& m_socket;
-        std::string& m_buffer;
+        virtual void connect(IPv4Address address, Port port, const std::string& hostname, Error& error) noexcept = 0;
+        virtual int read(char* buffer, int length, Error& error) = 0;
+        virtual void write(const char* buffer, int length, Error& error) = 0;
     };
 
-    // TODO: I need to put this somewhere else and make user configurable
-    class BotanCredentialsManager : public Botan::Credentials_Manager
-    {
-    public:
-        BotanCredentialsManager();
-
-        std::vector<Botan::Certificate_Store*> trusted_certificate_authorities(const std::string& type,
-            const std::string& context) override;
-        std::vector<Botan::X509_Certificate> cert_chain(const std::vector<std::string>& cert_key_types,
-            const std::string& type, const std::string& context) override;
-        Botan::Private_Key* private_key_for(const Botan::X509_Certificate& cert, const std::string& type,
-            const std::string& context) override;
-
-    private:
-        std::vector<Botan::Certificate_Store*> m_stores;
-    };
-
-    TCPClientSocket m_socket;
-    BotanTLSCallbacks m_botanTLSCallbacks;
-    // TODO: what is the autoseed RNG, is it safe enough
-    Botan::AutoSeeded_RNG m_rng;
-    // TODO: do I need this although I said the session is never cached above?
-    Botan::TLS::Session_Manager_In_Memory m_sessionManager;
-    BotanCredentialsManager m_credentials;
-    Botan::TLS::Strict_Policy m_policy;
-    // TODO: I needed to make this a pointer because the port is only known when connect(...) is called.
-    std::unique_ptr<Botan::TLS::Client> m_tlsClient;
-    std::string m_buffer;
+    Impl* m_impl;
 };
 
 }
--- src/TLSClientSocketBotanClientImpl.cpp
+++ src/TLSClientSocketBotanClientImpl.cpp
@@ -5,3 +5,172 @@
 */
 
 #include "TLSClientSocketBotanClientImpl.hpp"
+#include <botan/x509path.h>
+
+using namespace Ishiko;
+
+TLSClientSocketBotanClientImpl::TLSClientSocketBotanClientImpl(Error& error) noexcept
+    : m_socket(error), m_botanTLSCallbacks(m_socket, m_buffer),
+    m_sessionManager(m_rng)
+{
+}
+
+void TLSClientSocketBotanClientImpl::connect(IPv4Address address, Port port, const std::string& hostname,
+    Error& error) noexcept
+{
+    m_socket.connect(address, port, error);
+
+    // TODO: replace "needfulsoftware.com" with correct domain but how?
+    // TODO: I want TLS v.1.3 but only available in Botan 3.0.0. which is not released yet
+    // TODO: what of TLS 1.2 is not supported, can I safely downgrade?
+    m_tlsClient.reset(new Botan::TLS::Client(m_botanTLSCallbacks, m_sessionManager, m_credentials, m_policy, m_rng,
+        Botan::TLS::Server_Information(hostname, port.number()), Botan::TLS::Protocol_Version::TLS_V12));
+
+    // When the Botan TLS client is active it means the handshake has finished and we can start sending data so we
+    // consider the connection has now been established and we return to the caller.
+    while (!m_tlsClient->is_closed() && !m_tlsClient->is_active())
+    {
+        // TODO: buffer size?
+        Buffer buffer(10 * 1024);
+        // TODO: handle error
+        int n = m_socket.read(buffer, buffer.capacity(), error);
+
+        // TODO: is there not an issue that I may read data from the server past the handshake. So application data
+        // might be called. This would only happen if the server pushes data. So I need to buffer everything received
+        // by tls_record_received
+
+        // TODO: this returns the amount of data needed to complete the next record. Do I need to do something with
+        // this.
+        size_t needed = m_tlsClient->received_data((const uint8_t*)buffer.data(), n);
+    }
+}
+
+int TLSClientSocketBotanClientImpl::read(char* buffer, int length, Error& error)
+{
+    // TODO: how much TLS encoded data do I need to read to get the amount requested by the client?
+    // I'll never know I think. So I can only read into an internal buffer and return data from that.
+
+    // TODO: handle error
+    if (!m_buffer.empty())
+    {
+        size_t count = m_buffer.size();
+        if (m_buffer.size() > length)
+        {
+            count = length;
+        }
+        memcpy(buffer, m_buffer.c_str(), count);
+        m_buffer.erase(m_buffer.begin(), (m_buffer.begin() + count));
+        return count;
+    }
+
+    // TODO: buffer size?
+    Buffer localBuffer(10 * 1024);
+    // TODO: handle error
+    int n = m_socket.read(localBuffer, localBuffer.capacity(), error);
+    if (n == 0)
+    {
+        // TODO: socket was closed on the other end
+        return 0;
+    }
+
+    size_t needed = m_tlsClient->received_data((const uint8_t*)localBuffer.data(), n);
+    // TODO: take into account needed
+
+    // TODO: is it possible here that I read so little I don't have a full TLS packet and m_buffer is still empty but
+    // the socket is not closed.
+    if (!m_buffer.empty())
+    {
+        size_t count = m_buffer.size();
+        if (m_buffer.size() > length)
+        {
+            count = length;
+        }
+        memcpy(buffer, m_buffer.c_str(), count);
+        m_buffer.erase(m_buffer.begin(), (m_buffer.begin() + count));
+        return count;
+    }
+
+    return 0;
+}
+
+void TLSClientSocketBotanClientImpl::write(const char* buffer, int length, Error& error)
+{
+    // TODO: how do we handle errors?
+    m_tlsClient->send((const uint8_t*)buffer, length);
+}
+
+TLSClientSocketBotanClientImpl::BotanTLSCallbacks::BotanTLSCallbacks(TCPClientSocket& socket, std::string& buffer)
+    : m_socket(socket), m_buffer(buffer)
+{
+}
+
+void TLSClientSocketBotanClientImpl::BotanTLSCallbacks::tls_emit_data(const uint8_t data[], size_t size)
+{
+    // TODO: how do I handle errors
+    Error error;
+    m_socket.write((const char*)data, size, error);
+}
+
+void TLSClientSocketBotanClientImpl::BotanTLSCallbacks::tls_record_received(uint64_t seq_no, const uint8_t data[], size_t size)
+{
+    m_buffer.append((const char*)data, size);
+}
+
+void TLSClientSocketBotanClientImpl::BotanTLSCallbacks::tls_alert(Botan::TLS::Alert alert)
+{
+    // TODO: how do I handle all possible alerts. One of those is in fact "close"
+}
+
+bool TLSClientSocketBotanClientImpl::BotanTLSCallbacks::tls_session_established(const Botan::TLS::Session& session)
+{
+    // Do not cache the session
+    // TODO: do I need to support this?
+    return false;
+}
+
+void TLSClientSocketBotanClientImpl::BotanTLSCallbacks::tls_verify_cert_chain(
+    const std::vector<Botan::X509_Certificate>& cert_chain,
+    const std::vector<std::shared_ptr<const Botan::OCSP::Response>>& ocsp,
+    const std::vector<Botan::Certificate_Store*>& trusted_roots, Botan::Usage_Type usage, const std::string& hostname,
+    const Botan::TLS::Policy& policy)
+{
+    if (cert_chain.empty())
+    {
+        throw Botan::Invalid_Argument("Certificate chain was empty");
+    }
+
+    Botan::Path_Validation_Restrictions restrictions(policy.require_cert_revocation_info(),
+        policy.minimum_signature_strength());
+    auto ocsp_timeout = std::chrono::milliseconds(1000);
+    Botan::Path_Validation_Result result =
+        Botan::x509_path_validate(cert_chain, restrictions, trusted_roots, hostname, usage,
+            std::chrono::system_clock::now(), ocsp_timeout, ocsp);
+}
+
+TLSClientSocketBotanClientImpl::BotanCredentialsManager::BotanCredentialsManager()
+{
+    // TODO: is this adequate?
+    // We use the system managed trusted CA list
+    m_stores.push_back(new Botan::System_Certificate_Store);
+}
+
+std::vector<Botan::Certificate_Store*>
+TLSClientSocketBotanClientImpl::BotanCredentialsManager::trusted_certificate_authorities(const std::string& type,
+    const std::string& context)
+{
+    return m_stores;
+}
+
+std::vector<Botan::X509_Certificate> TLSClientSocketBotanClientImpl::BotanCredentialsManager::cert_chain(
+    const std::vector<std::string>& cert_key_types, const std::string& type, const std::string& context)
+{
+    // We don't use client authentication
+    return std::vector<Botan::X509_Certificate>();
+}
+
+Botan::Private_Key* TLSClientSocketBotanClientImpl::BotanCredentialsManager::private_key_for(const Botan::X509_Certificate& cert,
+    const std::string& type, const std::string& context)
+{
+    // We don't use client authentication
+    return nullptr;
+}
