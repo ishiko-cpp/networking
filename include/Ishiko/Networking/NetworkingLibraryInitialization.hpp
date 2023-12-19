a9f8fefa5c16e2694f3bdb4a35505f0baaef54f7
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-12-19 19:31:49 +0000

    Update tests

--- include/Ishiko/Networking/NetworkingLibraryInitialization.hpp
+++ include/Ishiko/Networking/NetworkingLibraryInitialization.hpp
@@ -1,13 +1,13 @@
 /*
-    Copyright (c) 2022 Xavier Leclercq
+    Copyright (c) 2022-2023 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
 */
 
-#ifndef _ISHIKO_CPP_NETWORKING_NETWORKINGLIBRARYINITIALIZATION_HPP_
-#define _ISHIKO_CPP_NETWORKING_NETWORKINGLIBRARYINITIALIZATION_HPP_
+#ifndef GUARD_ISHIKO_CPP_NETWORKING_NETWORKINGLIBRARYINITIALIZATION_HPP
+#define GUARD_ISHIKO_CPP_NETWORKING_NETWORKINGLIBRARYINITIALIZATION_HPP
 
-#include <Ishiko/BasePlatform.h>
+#include <Ishiko/BasePlatform.hpp>
 
 #if ISHIKO_OS == ISHIKO_OS_LINUX
     #include "linux/NetworkingLibraryInitialization.hpp"
