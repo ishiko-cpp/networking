d4c49b287c291225daab78a4ff7b7ff46e42ce7b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-09-27 12:21:29 +0100

    Update NativeSocketHandle.hpp

--- include/Ishiko/Networking/windows/NativeSocketHandle.hpp
+++ include/Ishiko/Networking/windows/NativeSocketHandle.hpp
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2021-2024 Xavier Leclercq
 // SPDX-License-Identifier: BSL-1.0
 
-#ifndef GUARD_ISHIKO_CPP_NETWORKING_NATIVESOCKETHANDLE_HPP
-#define GUARD_ISHIKO_CPP_NETWORKING_NATIVESOCKETHANDLE_HPP
+#ifndef GUARD_ISHIKO_CPP_NETWORKING_WINDOWS_NATIVESOCKETHANDLE_HPP
+#define GUARD_ISHIKO_CPP_NETWORKING_WINDOWS_NATIVESOCKETHANDLE_HPP
 
 #define NOMINMAX // TODO: what rubbish, how do I put this ugly windows logic somewhere central
 #include <winsock2.h>
