5b54c8dac48f5741ebfb1e9f5b7fb764c2a593af
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-10-05 15:26:41 +0100

    Updates

--- include/Ishiko/Networking/NetworkConnectionsManager.hpp
+++ include/Ishiko/Networking/NetworkConnectionsManager.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021-2024 Xavier Leclercq
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
 // SPDX-License-Identifier: BSL-1.0
 
 #ifndef GUARD_ISHIKO_CPP_NETWORKING_NETWORKCONNECTIONSMANAGER_HPP
--- include/Ishiko/Networking/TLSServerSocket.hpp
+++ include/Ishiko/Networking/TLSServerSocket.hpp
@@ -1,11 +1,8 @@
-/*
-    Copyright (c) 2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/networking/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP_
-#define _ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP_
+#ifndef GUARD_ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP
+#define GUARD_ISHIKO_CPP_NETWORKING_TLSSERVERSOCKET_HPP
 
 #include "TCPServerSocket.hpp"
 #include "TLSClientSocket.hpp"
@@ -14,24 +11,28 @@
 
 namespace Ishiko
 {
+    class TLSServerSocket
+    {
+    public:
+        TLSServerSocket(IPv4Address address, Port port, std::string keyPath, std::string certificatePath,
+            Error& error) noexcept;
+        TLSServerSocket(const TLSServerSocket& other) = delete;
+        TLSServerSocket& operator=(const TLSServerSocket& other) = delete;
+        // TODO: I probably want to allow the move constructor
+        TLSServerSocket(TLSServerSocket&& other) noexcept = delete;
+        TLSServerSocket& operator=(TLSServerSocket&& other) = delete;
+        ~TLSServerSocket() = default;
 
-class TLSServerSocket
-{
-public:
-    TLSServerSocket(IPv4Address address, Port port, std::string keyPath, std::string certificatePath,
-        Error& error) noexcept;
+        TLSClientSocket accept(Error& error) noexcept;
 
-    TLSClientSocket accept(Error& error) noexcept;
+        IPAddress ipAddress() const;
+        Port port() const;
 
-    IPAddress ipAddress() const;
-    Port port() const;
-
-private:
-    TCPServerSocket m_socket;
-    std::string m_keyPath;
-    std::string m_certificatePath;
-};
-
+    private:
+        TCPServerSocket m_socket;
+        std::string m_keyPath;
+        std::string m_certificatePath;
+    };
 }
 
 #endif
