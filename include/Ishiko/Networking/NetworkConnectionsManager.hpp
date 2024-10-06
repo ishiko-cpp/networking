9b9362ac3e0aee2c26d9cf57a841a1fb98e49cf2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-10-06 17:31:52 +0100

    Updates

--- include/Ishiko/Networking/NetworkConnectionsManager.hpp
+++ include/Ishiko/Networking/NetworkConnectionsManager.hpp
@@ -8,7 +8,6 @@
 #include "IPv4Address.hpp"
 #include "NativeSocketHandle.hpp"
 #include "Port.hpp"
-#include "TCPClientSocket.hpp"
 #include "TLSClientSocket.hpp"
 #include <Ishiko/Errors.hpp>
 #include <Ishiko/Memory.hpp>
@@ -48,31 +47,6 @@ namespace Ishiko
             void* m_impl;
         };
 
-        // TODO: find a better name for this, although maybe it's OK?
-        // TODO: Need to be able to close and shutdown the connection
-        class ManagedTLSSocket
-        {
-        public:
-            virtual void handshake(Error& error) = 0;
-
-            virtual int read(char* buffer, int count, Error& error) = 0;
-
-            virtual void write(const char* buffer, int count, Error& error) = 0;
-
-            virtual void shutdown(Error& error) = 0;
-            virtual void close() = 0;
-        };
-
-        // TODO: Can I unify this with ConnectionCallbacks or is it better like this?
-        class TLSConnectionCallbacks
-        {
-        public:
-            virtual void onConnectionEstablished(ManagedTLSSocket& socket) = 0;
-            virtual void onHandshake() = 0;
-            virtual void onReadReady() = 0;
-            virtual void onWriteReady() = 0;
-        };
-
         NetworkConnectionsManager();
         NetworkConnectionsManager(const NetworkConnectionsManager& other) = delete;
         NetworkConnectionsManager& operator=(const NetworkConnectionsManager& other) = delete;
@@ -83,15 +57,10 @@ namespace Ishiko
         Registration registerSocketAndCallbacks(NativeSocketHandle socket_handle,
             void (*event_handler)(Event evt, void* data), void* event_handler_data);
 
-        void connectWithTLS(IPv4Address address, Port port, const Hostname& hostname,
-            TLSConnectionCallbacks& callbacks, Error& error);
-
         void run(bool (*stop_function)(NetworkConnectionsManager& connections_manager));
         bool idle() const;
 
     private:
-        class ManagedTLSSocketImpl;
-
         class RegistrationImpl
         {
         public:
@@ -113,66 +82,17 @@ namespace Ishiko
             void setWaitingForRead(RegistrationImpl* socket_and_callbacks);
             void setWaitingForWrite(RegistrationImpl* socket_and_callbacks);
 
-            void setWaitingForConnection(ManagedTLSSocketImpl* managed_socket);
-            void setWaitingForRead(ManagedTLSSocketImpl* managed_socket);
-            void setWaitingForWrite(ManagedTLSSocketImpl* managed_socket);
-
             std::set<RegistrationImpl*> m_new_waiting_for_connection3;
             std::set<RegistrationImpl*> m_new_waiting_for_read3;
             std::set<RegistrationImpl*> m_new_waiting_for_write3;
-
-            std::set<ManagedTLSSocketImpl*> m_new_waiting_for_connection2;
-            std::set<ManagedTLSSocketImpl*> m_new_waiting_for_read2;
-            std::set<ManagedTLSSocketImpl*> m_new_waiting_for_write2;
-        };
-
-        class ManagedTLSSocketImpl : public ManagedTLSSocket
-        {
-        public:
-            ManagedTLSSocketImpl(SharedState& shared_state, TLSClientSocket&& socket, TLSConnectionCallbacks& callbacks);
-
-            void connect(IPv4Address address, Port port, const Hostname& hostname, Error& error);
-
-            void handshake(Error& error) override;
-
-            int read(char* buffer, int count, Error& error) override;
-
-            void write(const char* buffer, int count, Error& error) override;
-
-            void shutdown(Error& error) override;
-            void close() override;
-
-            void callback();
-
-            TLSClientSocket& socket();
-
-        private:
-            enum class State
-            {
-                waiting_for_connection,
-                waiting_for_handshake,
-                waiting_for_read,
-                waiting_for_write
-            };
-            SharedState& m_shared_state;
-            TLSClientSocket m_socket;
-            TLSConnectionCallbacks& m_callbacks;
-            State m_state;
         };
 
         // TODO: replace this with stable collection, maybe a hive? Unless I make the clients of this class agnostic
         // of the actual memory location.
         std::vector<RegistrationImpl> m_registrations;
-
-        // TODO: replace this with stable collection, maybe a hive? Unless I make the clients of this class agnostic
-        // of the actual memory location.
-        std::vector<ManagedTLSSocketImpl> m_managed_tls_sockets;
     public: // TODO: back to private
         SharedState m_shared_state;
     private:
-        std::set<ManagedTLSSocketImpl*> m_waiting_for_connection2;
-        std::set<ManagedTLSSocketImpl*> m_waiting_for_read2;
-        std::set<ManagedTLSSocketImpl*> m_waiting_for_write2;
         std::set<RegistrationImpl*> m_waiting_for_connection3;
         std::set<RegistrationImpl*> m_waiting_for_read3;
         std::set<RegistrationImpl*> m_waiting_for_write3;
--- src/AsyncTLSClientSocket.cpp
+++ src/AsyncTLSClientSocket.cpp
@@ -2,45 +2,138 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include "AsyncTLSClientSocket.hpp"
+#include "NetworkingErrorCategory.hpp"
 
 using namespace Ishiko;
 
-void AsyncTLSClientSocket::Callbacks::onConnectionEstablished(NetworkConnectionsManager::ManagedTLSSocket& socket)
-{
-    m_socket = &socket;
-
-    // TODO: error?
-    Error error;
-    onConnectionEstablished(error);
-}
-
-void AsyncTLSClientSocket::Callbacks::onHandshake()
-{
-}
-
-void AsyncTLSClientSocket::Callbacks::onReadReady()
-{
-}
-
-void AsyncTLSClientSocket::Callbacks::onWriteReady()
-{
-}
-
 AsyncTLSClientSocket::AsyncTLSClientSocket(NetworkConnectionsManager& connections_manager, Callbacks& callbacks,
     Error& error) noexcept
-    : m_connections_manager{connections_manager}, m_callbacks{callbacks}
+    : m_socket{SocketOption::non_blocking, error}, m_callbacks{callbacks}, m_state{State::init}
 {
-    // TODO: socket creation should happen here, none of that managed socket stuff
+    if (!error)
+    {
+        // TODO: need to unregister somewhere
+        m_registration = connections_manager.registerSocketAndCallbacks(m_socket.socket().nativeHandle(), EventHandler,
+            this);
+    }
 }
 
 void AsyncTLSClientSocket::connect(IPv4Address address, Port port, const Hostname& hostname) noexcept
 {
+    // TODO: error handling
     Error error;
-    m_connections_manager.connectWithTLS(address, port, hostname, m_callbacks, error);
-    // TODO: even if there is no error the NetworkConnectionsManager will trigger the callback
+    m_socket.connect(address, port, hostname.asString(), error);
+    if (error)
+    {
+        if (error.code() == NetworkingErrorCategory::Value::would_block)
+        {
+            // TODO: thread safety
+            m_registration.setWaitingForConnection();
+        }
+    }
+    else
+    {
+        // TODO: we need to get the callback executed from the NetworkConnectionsManager::run function
+    }
+}
+
+void AsyncTLSClientSocket::handshake() noexcept
+{
+    // TODO: error handling
+    Error error;
+    m_socket.handshake(error);
+    if (error)
+    {
+        if (error.code() == NetworkingErrorCategory::Value::would_block)
+        {
+            // TODO: not sure but this may cause a race condition with run() since state and the call are separate steps
+            // TODO: make this work with TLS
+            m_state = State::waiting_for_handshake;
+            m_registration.setWaitingForRead();
+        }
+    }
+}
+
+int AsyncTLSClientSocket::read(char* buffer, int length)
+{
+    // TODO: error handling
+    Error error;
+    int n = m_socket.read(buffer, length, error);
+    if (error)
+    {
+        if (error.code() == NetworkingErrorCategory::Value::would_block)
+        {
+            // TODO: not sure but this may cause a race condition with run() since state and the call are separate steps
+            m_state = State::waiting_for_read;
+            m_registration.setWaitingForRead();
+        }
+    }
+    return n;
+}
+
+void AsyncTLSClientSocket::write(const char* buffer, int length)
+{
+    // TODO: error handling
+    Error error;
+    m_socket.write(buffer, length, error);
+    if (error)
+    {
+        if (error.code() == NetworkingErrorCategory::Value::would_block)
+        {
+            // TODO: not sure but this may cause a race condition with run() since state and the call are separate steps
+            m_state = State::waiting_for_write;
+            m_registration.setWaitingForWrite();
+        }
+    }
 }
 
 void AsyncTLSClientSocket::close() noexcept
 {
-    m_callbacks.m_socket->close();
+    m_socket.close();
+    // TODO: unregister
+}
+
+void AsyncTLSClientSocket::EventHandler(NetworkConnectionsManager::Event evt, void* data)
+{
+    // TODO: error handling
+    Error error;
+
+    AsyncTLSClientSocket* socket = static_cast<AsyncTLSClientSocket*>(data);
+    switch (evt)
+    {
+    case NetworkConnectionsManager::Event::connection_ready:
+        socket->m_callbacks.onConnectionEstablished(error, *socket);
+        break;
+
+    case NetworkConnectionsManager::Event::read_ready:
+        if (socket->m_state == State::waiting_for_handshake)
+        {
+            // TODO: if the socket gets connected here it means the connect call blocked and the TLS handshaked wasn't
+            // completed. Call onCallback to continue the handshake.
+            socket->m_socket.onCallback();
+            if (!socket->m_socket.isConnected())
+            {
+                // TODO
+                // TLS handshake hasn't finished
+                // TODO: could be waiting for a read or write, can I improve this?
+                socket->m_registration.setWaitingForRead();
+                // TODO: actually waiting for both definitely will cause trouble as the write will never be reset
+                // TODO: I definitely need to know which to wait on
+                //m_shared_state.setWaitingForWrite(this);
+            }
+            else
+            {
+                socket->m_callbacks.onHandshake(error, *socket);
+            }
+        }
+        else
+        {
+            socket->m_callbacks.onReadReady(error, *socket);
+        }
+        break;
+
+    case NetworkConnectionsManager::Event::write_ready:
+        socket->m_callbacks.onWriteReady(error, *socket);
+        break;
+    }
 }
